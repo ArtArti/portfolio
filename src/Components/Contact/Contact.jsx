890577651a97d68aa32f953119f8b67f139605fa
ArtArti <103097814+ArtArti@users.noreply.github.com>
2024-03-12 21:12:35 +0530

    Update Contact.jsx

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('https://gmail-system.vercel.app/api/send', {
+      const response = await fetch('https://gmail-system-five.vercel.app/api/send', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
