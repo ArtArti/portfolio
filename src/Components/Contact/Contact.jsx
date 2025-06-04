0a404516e8183ea09727073bc6d593934c77e8d9
Arti <singharti7648@gmail.com>
2025-06-05 00:34:48 +0530

    update contact

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -69,7 +69,7 @@ const Contact = () => {
     <section
       id="contact"
       className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative mx-8"
-    >
+    >   
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
         <h5 className=" text-xl font-bold  my-2">{t('contact.lets')}</h5>
@@ -139,6 +139,7 @@ const Contact = () => {
             <textarea
               name="message"
               id="message"
+              type="text"
               className="bg-[#d2d4dc] border border-[#33353F] placeholder-[#141414] text-sm rounded-lg block w-full p-2.5"
               placeholder="Let's talk about..."
                value={formData.text}
@@ -160,6 +161,15 @@ const Contact = () => {
         {isSubmitting ? 'Submitting...' : 'Submit Form'}
       </button>
         </form>
+         {submitMessage && (
+        <div className={` p-4 rounded-lg text-center w-96 my-3 ${
+          submitMessage.includes('Error') 
+            ? 'bg-red-100 text-red-700 border border-red-300' 
+            : 'bg-green-100 text-green-700 border border-green-300'
+        }`}>
+          {submitMessage}
+        </div>
+      )}
       </div>
     </section>
   );
